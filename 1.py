5d64948d83bdcf769cb4e46546cde91523226d99
Сергей <s.trafimov2006@mail.rugit config --listgit config --global user.email s.trafimov2006@mail.ru>
2023-06-15 20:24:42 +0300

    initialion

---
+++ 1.py
@@ -0,0 +1,21 @@
+'''def strcounter(s):
+    for syn in set(s):
+        count = 0
+        for supsym in s:
+            if syn == supsym:
+                count+=1
+        print(syn, count)
+
+str="abca"
+strcounter(str)'''
+
+
+def strcounter(s):
+    sym_count={}
+    for sym in s:
+        sym_count[sym] = sym_count.get(sym, 0) +1
+    for sym, count in sym_count.items():
+        print(sym, count)
+
+
+strcounter('hfhfhfhfhfhhfhfsjekusaqwe')
